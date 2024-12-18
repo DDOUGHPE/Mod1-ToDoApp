1f7a8489de59571b78f11d65039cde3a0eba2cb9
Tim Sawyer <tsawyer94til@gmail.com>
2024-12-18 09:26:18 -0600

    'Final'

--- to_do_final.py
+++ to_do_final.py
@@ -1,8 +1,8 @@
 tasks = []
-index = 1
+# index = 1
 
-rule_log = '''\n type "add" to add a task to your list
-    \n type "del" to remove a task'
+rule_log = '''\n type "add" to add a task to your To Do list
+    \n type "del" to remove a task from your To Do List'
     \n type "view" to view your To Do List'
     \n type "done" to display your finished To Do List!'''
 
@@ -13,44 +13,52 @@ def addtask():
     print(f"'{task}' has been added to the list")
 
 def listtask():
-    if not tasks:
-        print('no tasks saved yet')
-    else:
-        print(f' here are your tasks:.1')
+        print(f'here are your tasks:')
         for index, task in enumerate(tasks, start=1):
-            print(f'Task {index}. {task}')
-
+            print(f'Task {index}. {task}'.title())
 
 def del_task():
     listtask()
-    try:
-        tasktodel = int(input('Which task do you want to delete-> '))
-        index + 1
-        if tasktodel >= 0 and tasktodel < len(tasks):
-            tasks.pop(tasktodel)
-            print(f"{tasktodel} has been removed from the list")
-    except:
-        print('INVALID')
+    tasktodel = int(input('Which task do you want to delete-> '))
+    up_tasktodel = int(tasktodel - 1)
+    rem_task = tasks.pop(up_tasktodel)
+    print(f"{rem_task} has been removed from the list")
 
 
-name = input('whats your name?: ')
+
+name = input('TO CREATE A TO DO LIST PLEASE ENTER YOUR NAME: ')
 print('\nHello', name.title(), " Welcome to your To Do List:")
 while True:
-    print(rule_log)
-    action = input('\n->').split()
-    if action == "add".split():
-        addtask()
-    elif action == 'del'.split():
-        del_task()
-    elif action == 'view'.split():
-        listtask()
-    elif action == 'done'.split(): 
-        if not tasks:
-            print('No tasks created')
+    try:
+        print(rule_log)
+        action = input('\n->').split()
+        if action == "add".split():
+            addtask()
+        elif action == 'del'.split():
+                if len(tasks) <= 0:
+                    print('No Task Created')
+                else:
+                    try:
+                        del_task()
+                    except IndexError:
+                        print('NOT A VALID TASK NUMBER')
+        elif action == 'view'.split():
+            try: 
+                if len(tasks) <= 0:
+                    print('No Task Created')
+                else:
+                    listtask()
+            except ValueError:
+                print('Invalid Input')
+        elif action == 'done'.split(): 
+            if not tasks:
+                print('No tasks created')
+            else:
+                print('Check out your To Do List', name.title(),)
+                print('-' * 30)
+                listtask()
+            break
         else:
-            print('Check out your To Do List', name.title(),)
-            print('-' * 30)
-            print(tasks)
-        break
-    else:
-        print('INVALID')
+            print('NOT A OPTION')
+    except ValueError:
+        print('INVALID INPUT')
